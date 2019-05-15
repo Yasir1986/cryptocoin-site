fa19432feacef3a0ca33fad233887861b69a7b9e
Yasir Akbar <yasir.akbar86@hotmail.com>
2019-05-15 15:04:36 +0300

    fix

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,47 +1,58 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Button from '@material-ui/core/Button';
-import styled from 'styled-components';
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Button from "@material-ui/core/Button";
+import styled from "styled-components";
 import { Link } from "react-router-dom";
-
 
 const styles = {
   root: {
-    flexGrow: 1,
+    flexGrow: 1
   },
-};
+  };
 
 function ButtonAppBar(props) {
   const { classes } = props;
   return (
     <div className={classes.root}>
       <AppBar position="static">
-      <StyledNav>
+        <StyledNav>
           <Toolbar>
-   <Button color="inherit">Home</Button>
-   <Link to="/about">   <Button color="inherit">About</Button></Link>
-   <Link to="/faq">       <Button color="inherit">F.A.Q</Button></Link> 
-   <Link to="/contact">      <Button color="inherit">Contact</Button></Link>
+            <Link to="/">
+              <Button>Home</Button>
+            </Link>
+            <Link to="/about">
+              {" "}
+              <Button>About</Button>
+            </Link>
+            <Link to="/faq">
+              {" "}
+              <Button>F.A.Q</Button>
+            </Link>
+            <Link to="/contact">
+              {" "}
+              <Button>Contact</Button>
+            </Link>
           </Toolbar>
-          </StyledNav>
+        </StyledNav>
       </AppBar>
-     
     </div>
   );
 }
 
 ButtonAppBar.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(ButtonAppBar);
 
 const StyledNav = styled.div`
-    display: flex;
-    position: relative;
-    align-items: center;
-    justify-content: flex-end;
+  display: flex;
+  position: relative;
+  align-items: center;
+  justify-content: flex-end;
 `;
+
+
