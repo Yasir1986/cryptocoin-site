b1346fd390445722b20d4541c5c35f41235ca6a7
Yasir Akbar <yasir.akbar86@hotmail.com>
2019-05-22 19:59:16 +0300

    Eth page added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import Home from './components/Home';
 import About from './components/About';
 import FAQ from "./components/FAQ";
 import Contact from "./components/Contact";
-import Btc from "./components/Btc"
+import Btc from "./components/Btc";
+import Eth from "./components/Eth";
 import { BrowserRouter as Router, Route  } from "react-router-dom";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path="/about" component={About} />
         <Route path="/faq" component={FAQ} />
         <Route path="/btc" component={Btc} />
+        <Route path="/eth" component={Eth} />
         <Route path="/contact" component={Contact} />
     </Router>
   );
